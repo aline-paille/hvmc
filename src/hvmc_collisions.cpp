19950ac8297d2cd103dd1294448f866c3dc93e42
aline <aline.lepailleur@gmail.com>
2015-10-16 15:42:51 +0200

    fonction circles

--- src/hvmc_collisions.cpp
+++ src/hvmc_collisions.cpp
@@ -6,9 +6,13 @@ bool CollideBoxes(const RigidBody &a, const RigidBody &b, CollisionInfo &info){
 }
 
 bool CollideCircles(const RigidBody &a, const RigidBody &b, CollisionInfo &info){
-
+    f32 raduisSum = pow(a.collider.radius  + b.collider.radius, 2);
+    f32 distCenters = pow(a.position.x - b.position.x, 2) + pow(a.position.y + b.position.y,2);
+    if(raduisSum > distCenters)
+        return true;
     return false;
 }
+
 
 bool CollideBoxCircle(const RigidBody &a /*Box*/, const RigidBody &b, CollisionInfo &info){
     vec2 extent;
